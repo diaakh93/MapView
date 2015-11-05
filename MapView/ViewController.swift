5cb109e57790518d5e51beb1f5acb7ffc19ff352
Diaa Khalil <diaakhalil93@gmail.com>
2015-11-05 13:41:25 -0500

    MapView testing

--- MapView/ViewController.swift
+++ MapView/ViewController.swift
@@ -6,13 +6,24 @@
 //  Copyright © 2015 Diaa. All rights reserved.
 //
 
+import MapKit
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        var centerLocation = CLLocationCoordinate2DMake(40.886409, -74.142482)
+        
+        var mapSpan = MKCoordinateSpanMake(0.01, 001)
+        
+        var mapRegion = MKCoordinateRegionMake(centerLocation, mapSpan)
+        
+        self.map.setRegion(mapRegion, animated: true)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
